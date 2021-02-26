2742b4898e348a295cff56d3fa5cff1c5d58f0fa
Sreekaran Srinath <sreekaran.srinath@gmail.com>
2021-02-26 19:30:37 +0530

    Now using json and keeping track of followers

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ driver = webdriver.Chrome(
     executable_path="/home/krn/repos/chromedriver/chromedriver", options=options
 )
 driver.maximize_window()
+total_group_growth = 0
+
+with open('tracker.json') as json_file:
+    tracker = json.load(json_file)
 
 
 def login(user, pw):
@@ -37,16 +41,21 @@ def login(user, pw):
 
 
 def get_followers(user):
+    global total_group_growth
     driver.get(f"https://twitter.com/{user}")
     sleep(3)
     follower_count = driver.find_element_by_xpath(
         "/html/body/div/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div/div[1]/div/div[5]/div[2]/a/span[1]/span"
-    ).text
-    return update_slack(user.strip(), follower_count)
+    ).text.replace(',', '')
+    user = user.strip()
+    tracker[user].append(follower_count)
+    growth = int(follower_count) - int(tracker[user][-2])
+    total_group_growth += growth
+    return update_slack(user, follower_count, growth)
 
 
-def update_slack(user, follower_count):
-    message = f"@{user} has {follower_count} followers."
+def update_slack(user, follower_count, growth):
+    message = f"@{user} has {follower_count} followers, up {growth} followers from {tracker[user][-2]} yesterday."
     response = requests.post(
         webhook_url,
         data=json.dumps({"text": message}),
@@ -61,6 +70,18 @@ def update_slack(user, follower_count):
 
 login(accountusername, accountpassword)
 lines = open("users.txt", "r").readlines()
-for line in lines:
-    get_followers(line)
+for key in tracker.keys():
+    get_followers(key)
     sleep(2)
+
+# Update the webhook with Total Group Growth
+message = f"In total, the group has grown by {total_group_growth} followers in the last 24 hours."
+response = requests.post(
+    webhook_url,
+    data=json.dumps({"text": message}),
+    headers={"Content-Type": "application/json"},
+)
+
+
+with open('tracker.json', 'w') as outfile:
+    json.dump(tracker, outfile)
